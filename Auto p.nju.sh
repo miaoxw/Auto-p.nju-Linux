55e6819e683342b64b5f2fdc669e55636c914995
Miau Shiauwe <mxw_19940830@sina.com>
2016-06-25 13:15:28 +0800

    Logical modification for NIC password policy

--- Auto p.nju.sh
+++ Auto p.nju.sh
@@ -5,18 +5,23 @@ password=********
 
 #获取在线状态
 statusCode=$(curl "http://p.nju.edu.cn/portal_io/getinfo"|jq ".reply_code")
-if [ $statusCode -eq 0 }
+if [ $statusCode -eq 0 ]
 then
 	echo "已经登录！"
 	exit 0
 else
 	statusCode=0
-	failCount=0
+	failCount=-1
 	until [ $statusCode -eq 1 -o $failCount -ge 5 ]
 	do
-		statusCode=$(curl -d "username=$username&password=$password" "http://p.nju.edu.cn/portal_io/login"|jq ".reply_code") || failCount=$(($failCount+1))
+		failCount=$(($failCount+1))
+		statusCode=$(curl -d "username=$username&password=$password" "http://p.nju.edu.cn/portal_io/login"|jq ".reply_code")
+		if [ $statusCode -eq 253 ]
+		then
+			sleep 1
+		fi
 	done
-	if [ $failCount -eq 1 ]
+	if [ $statusCode -eq 1 ]
 	then
 		echo "登录成功！"
 		exit 0
